ec5a5c7cca38c215678e8d3eb44a8f64d1f77da7
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-14 16:05:53 -0300

    :sparkles: [feat] application-done

--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -1,5 +1,8 @@
 package com.betrybe.bankaccount;
 
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * A class.
  */
@@ -9,7 +12,19 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    AccountNumberFormatter accountNumberFormatter = new AccountNumberFormatter();
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Informe o número da conta:");
+    String accountNumber = scanner.nextLine();
+
+    if (!StringUtils.isNumeric(accountNumber)) {
+      System.out.println("Número da conta inválido!");
+      return;
+    }
+
+    String returnAccountNumber = accountNumberFormatter.formatAccountNumber(Integer.parseInt(accountNumber));
+    System.out.println("Número da conta: " + returnAccountNumber);
   }
 
 }
